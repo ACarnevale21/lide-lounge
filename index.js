7a7132e4433edabf7e37c3541926468250300770
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-13 02:00:14 -0300

    backend+logo

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configure middleware to process form data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(require("./routes/index"));
+
+// Configure middleware to process form data
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
