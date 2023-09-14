212d20287b642b0ecfbb3f845440d5276f3d3093
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-14 00:53:15 -0300

    agregado de get a imagenes

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const PORT = process.env.PORT || 3000;
 
 // Configure middleware to process form data
 app.use(express.urlencoded({ extended: false }));
-app.use("/public", express.static("public"));
+app.use("/public/assets", express.static("assets"));
 app.use(require("./routes/index"));
 
 // Configure middleware to process form data
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.get("/assets/:nombreImagen", (req, res) => {
+  const nombreImagen = req.params.nombreImagen;
+  res.sendFile(__dirname + "/assets/" + nombreImagen);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
