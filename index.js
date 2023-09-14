979548356c6f0c6a34a087ee2e7789338f6d2793
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-14 01:59:17 -0300

    deleted assets todo a public

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 // Configure middleware to process form data
 app.use(express.urlencoded({ extended: false }));
-app.use("/assets", express.static("assets"));
 app.use(require("./routes/index"));
 
 // Configure middleware to process form data
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-
-app.get("/assets/:nombreImagen", (req, res) => {
-  const nombreImagen = req.params.nombreImagen;
-  res.sendFile(__dirname + "/assets/" + nombreImagen);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
