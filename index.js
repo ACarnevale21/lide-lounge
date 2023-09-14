291fcb2fbb9a2f1a649600d30a47c07e7e46b968
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-14 02:27:52 -0300

    a ver

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes/index"));
 
-// Configure middleware to process form data
+// Configurar middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
+
+// Ruta para servir imágenes estáticas desde la carpeta "public"
+app.get("/public/:img", (req, res) => {
+  const img = req.params.img;
+  res.sendFile(path.join(__dirname, "public", img));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
