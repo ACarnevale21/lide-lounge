5c250c565f99287e8a64c04435a051871cce0fbc
AgustinCarnevaleEscritorio <carnevaleagustinj@gmail.com>
2023-09-14 00:57:07 -0300

    a ver ahora sacanto /public

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Configure middleware to process form data
 app.use(express.urlencoded({ extended: false }));
-app.use("/public/assets", express.static("assets"));
+app.use("/assets", express.static("assets"));
 app.use(require("./routes/index"));
 
 // Configure middleware to process form data
